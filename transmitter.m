c2c0b1a21a5a406747480c65a62880e22bda4da1
jbcdnr <jb.cdnr@gmail.com>
2014-10-25 19:27:26 -0400

    Modified constants in transmitter

--- transmitter.m
+++ transmitter.m
@@ -1,21 +1,22 @@
-T = 100; % samples per symbol
+carrierFrequency = 10^5; % Hz
+symbolRate = 10^3; % Hz
+samplesPerSymbol = carrierFrequency / symbolRate;
 
 freqSync = ones(1, 20);
 timingSync = [1 1 -1 1 1 -1 -1 1 1 1 -1 1 1 -1 1 -1 -1 1 1 1];
-frameSync = [-1 -1 -1 -1 1 1 1 -1 -1 1 -1 -1 1 1 1 -1 -1 -1 -1];
+frameSync = [1 -1 1 1 -1 -1 1 1 1 -1 -1 -1 1 1 1 1 -1 -1 -1 -1 1 1 1 1];
 pilot = [1 -1 1 -1 1 -1 1 -1 1 -1];
 message = [1];
 
-
 symbols = [freqSync timingSync frameSync pilot message];
 
-nSamples = T*(length(symbols) + 1);
+nSamples = samplesPerSymbol * length(symbols);
 Xi = 1:nSamples;
 X = zeros(1, nSamples);
 
 for i = 1:length(symbols)
-    t = T*i;
-    X = X + symbols(i) * rectpuls(Xi - t, T);
+    t = samplesPerSymbol*i;
+    X = X + symbols(i) * rectpuls(Xi - t, samplesPerSymbol);
 end
 
 plot(Xi, X);
