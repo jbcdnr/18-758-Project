ad195bd16348eb7096beef10988d25159c05ca8b
jbcdnr <jb.cdnr@gmail.com>
2014-10-27 13:13:06 -0400

    frame sync and equalization implementation (not tested)

---
+++ equalize.m
@@ -0,0 +1,10 @@
+function [message, h0] = equalize(pilot, signal)
+% equalize the signal using the pilot sequence at the begining
+% and return the following equalized message
+% pilot | message | ... -> message_eq | ..._eq
+    
+    receivedPilot = signal(1:length(pilot));
+    h0 = (pilot .* receivedPilot) / (pilot .* pilot);
+    message = h0 * signal(length(pilot)+1:length(signal));
+    
+end
