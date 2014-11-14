83d8a00ab6708d1637d1f0ef98e02705e4c32f94
Tom Mullins <tmullins@andrew.cmu.edu>
2014-11-14 15:41:37 -0500

    Added params file for use in TX and RX

---
+++ params.m
@@ -0,0 +1,23 @@
+% signal timing
+txSamplingFrequency =  100 * 10^6; % Hz
+rxSamplingFrequency =   25 * 10^6; % Hz
+symbolRate          = 12.5 * 10^6; % Hz
+
+% signal parameters
+M = 4;        % size of constelation
+alpha = 0.25; % SRRC coefficient
+txPad = 32;   % extra 0 symbols to transmit on either side of message
+
+% receive paramters
+rxUpsample = 4;
+
+% transmit signal sync sequences
+freqSync = ones(1, 40);
+timingSync = [1 1 0 1 1 0 0 1 1 1 0 1 1 0 1 0 0 1 1 1]*2-1;
+frameSync = [0 0 0 0 1 0 0 1 1 1 0 0 0 0 1 1 1 1 1 0 0 0 0 1 1 1 0 0 1 0 0 0 0]*2-1;
+pilot = ones(1, 10);
+messageBits = [0 1 0 1 1 1 0 1 0 1 0 0 0 1 0 0 1 0 1 0 1 0 1 1 1 1 0 1 1 1 0 0 0 0 1 1 0 1 0 1];
+messageBits = [messageBits messageBits messageBits messageBits];
+messageBits = [messageBits messageBits messageBits messageBits];
+
+messageSize = length(messageBits);
